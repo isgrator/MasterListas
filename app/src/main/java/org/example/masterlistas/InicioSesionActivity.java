667751a3aa49afce0db70d0da5deab96a9694100
Isabel <isgrator@gmail.com>
2017-11-15 20:10:09 +0100

    Uso de checkbox (pág. 49)

--- app/src/main/java/org/example/masterlistas/InicioSesionActivity.java
+++ app/src/main/java/org/example/masterlistas/InicioSesionActivity.java
@@ -2,6 +2,9 @@ package org.example.masterlistas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class InicioSesionActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class InicioSesionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
+    }
+
+    public void loguearCheckbox(View v) {
+        CheckBox recordarme= (CheckBox) findViewById(R.id.recordarme);
+        String s = getResources().getString(R.string.texto_recordar)+": " +
+                (recordarme.isChecked() ? getResources().getString(R.string.si) : getResources().getString(R.string.no));
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
     }
 }
