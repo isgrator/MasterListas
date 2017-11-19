327e79e74a6ac89439955edb44d322923ebb7e77
Isabel <isgrator@gmail.com>
2017-11-19 01:00:49 +0100

    Navigation Drawer "elástico" (pág.78)

--- app/src/main/java/org/example/masterlistas/ListasActivity.java
+++ app/src/main/java/org/example/masterlistas/ListasActivity.java
@@ -25,11 +25,16 @@ import android.view.View;
 import android.widget.Toast;
 
 
+import com.mxn.soul.flowingdrawer_core.ElasticDrawer;
+import com.mxn.soul.flowingdrawer_core.FlowingDrawer;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class ListasActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class ListasActivity extends AppCompatActivity {
+    //implements NavigationView.OnNavigationItemSelectedListener {
+    private FlowingDrawer mDrawer;
     private RecyclerView recycler;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager lManager;
@@ -39,7 +44,7 @@ public class ListasActivity extends AppCompatActivity implements NavigationView.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listas);
 
-        FloatingActionButton fab= (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -78,7 +83,8 @@ public class ListasActivity extends AppCompatActivity implements NavigationView.
         recycler.addOnItemTouchListener(
                 new RecyclerItemClickListener(ListasActivity.this,
                         new RecyclerItemClickListener.OnItemClickListener() {
-                            @Override public void onItemClick(View v, int position) {
+                            @Override
+                            public void onItemClick(View v, int position) {
                                 Intent intent = new Intent(ListasActivity.this,
                                         DetalleListaActivity.class);
                                 intent.putExtra("numeroLista", position);
@@ -98,8 +104,31 @@ public class ListasActivity extends AppCompatActivity implements NavigationView.
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-        // Navigation Drawer
-        DrawerLayout drawer = (DrawerLayout) findViewById(
+
+        // Navigation Drawer (ver punto 5 de la pág.78)
+        NavigationView navigationView = (NavigationView) findViewById(
+                R.id.vNavigation);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        Toast.makeText(getApplicationContext(), menuItem.getTitle(),
+                                Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                });
+        mDrawer = (FlowingDrawer) findViewById(R.id.drawerlayout);
+        mDrawer.setTouchMode(ElasticDrawer.TOUCH_MODE_BEZEL);
+        toolbar.setNavigationIcon(R.drawable.ic_menu_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDrawer.toggleMenu();
+            }
+        });
+
+        // Navigation Drawer antiguo
+        /*DrawerLayout drawer = (DrawerLayout) findViewById(
                 R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
                 drawer, toolbar, R.string.drawer_open, R.string. drawer_close);
@@ -111,12 +140,15 @@ public class ListasActivity extends AppCompatActivity implements NavigationView.
 
         Transition lista_enter = TransitionInflater.from(this)
                 .inflateTransition(R.transition.transition_lista_enter);
-        getWindow().setEnterTransition(lista_enter);
+        getWindow().setEnterTransition(lista_enter);*/
+
+
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
-    @Override
+    /*@Override
     public boolean onNavigationItemSelected(MenuItem item) {
+        //No hace falta este método estando setNavigationItemSelectedListener
         int id = item.getItemId();
         if (id == R.id.nav_1) {
             // …
@@ -129,16 +161,24 @@ public class ListasActivity extends AppCompatActivity implements NavigationView.
                 R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
-    }
+    }*/
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(
+        /*DrawerLayout drawer = (DrawerLayout) findViewById(
                 R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
+        }*/
+
+        //Código para FlowingDrawer (pág.78, punto 7)
+        if (mDrawer.isMenuVisible()) {
+            mDrawer.closeMenu();
+        } else {
+            super.onBackPressed();
         }
     }
+
 }
